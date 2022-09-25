297e99bd265bbf1a3d9656eec95ada2848a377da
ousama <ousawack@gmail.com>
2022-09-25 19:09:33 +0100

    created a homescreen

---
+++ screens/HomeScreen.jsx
@@ -0,0 +1,97 @@
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TextInput,
+  ScrollView,
+} from "react-native";
+import React, { useEffect, useLayoutEffect, useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import {
+  UserIcon,
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsVerticalIcon,
+} from "react-native-heroicons/outline";
+import Categories from "../components/Categories";
+import FeaturedRow from "../components/FeaturedRow";
+import sanityClient from "../sanity";
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+  const [featuredCategories, setFeaturedCategories] = useState([]);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "featured"] {
+        ...,
+        restaurants[]->{
+        ...,
+        dishes[]->{
+        ...,
+          }
+        }
+      }`
+      )
+      .then((data) => {
+        setFeaturedCategories(data);
+      });
+  }, []);
+
+  return (
+    <SafeAreaView className="bg-white pt-5 flex-1">
+      {/* Header */}
+      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <Image
+          source={{ uri: "https://links.papareact.com/wru" }}
+          className="h-10 w-10 bg-gray-200 rounded-full"
+        />
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-sm">Deliver Now!</Text>
+          <Text className="font-bold text-lg ">
+            Current Location
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
+        </View>
+        <UserIcon size={30} color="#00CCBB" />
+      </View>
+      {/* Search */}
+      <View className="flex-row items-center space-x-2 pb-2 mx-4">
+        <View className="flex-row space-x-2 flex-1 bg-gray-200 p-3 rounded-lg">
+          <MagnifyingGlassIcon size={25} color="gray" />
+          <TextInput
+            placeholder="Restaurants and cuisines"
+            keyboardType="default"
+          />
+        </View>
+        <AdjustmentsVerticalIcon color="#00CCBB" />
+      </View>
+      {/* Body */}
+      <ScrollView className="bg-gray-100">
+        {/* Categories */}
+        <Categories />
+        {/* Featured */}
+        {featuredCategories?.map((category) => {
+          return (
+            <FeaturedRow
+              key={category._id}
+              id={category._id}
+              title={category.name}
+              description={category.short_description}
+            />
+          );
+        })}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
